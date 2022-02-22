3d71f0defe0cabbd24655f9a488c1fc933ddcf01
Fraser Clark <fraser.kr@gmail.com>
2022-02-22 19:42:17 +0000

    fixing s3 aws provider 4.0.0

--- s3.tf
+++ s3.tf
@@ -1,8 +1,29 @@
 # S3 bucket for website.
 resource "aws_s3_bucket" "www_bucket" {
   bucket = "www.${var.bucket_name}"
-  acl    = "public-read"
   policy = templatefile("${path.module}/files/s3-policy.json", { bucket = "www.${var.bucket_name}" })
+
+}
+
+resource "aws_s3_bucket_acl" "www_bucket" {
+  bucket = aws_s3_bucket.www_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "www_bucket" {
+  bucket = aws_s3_bucket.www_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "www_bucket" {
+  bucket = aws_s3_bucket.www_bucket.id
 
   cors_rule {
     allowed_headers = ["Authorization", "Content-Length"]
@@ -10,20 +31,24 @@ resource "aws_s3_bucket" "www_bucket" {
     allowed_origins = ["https://www.${var.domain_name}"]
     max_age_seconds = 3000
   }
-
-  website {
-    index_document = "index.html"
-    error_document = "404.html"
-  }
 }
 
 # S3 bucket for redirecting non-www to www.
 resource "aws_s3_bucket" "root_bucket" {
   bucket = var.bucket_name
-  acl    = "public-read"
   policy = templatefile("${path.module}/files/s3-policy.json", { bucket = var.bucket_name })
 
-  website {
-    redirect_all_requests_to = "https://www.${var.domain_name}"
+}
+
+resource "aws_s3_bucket_acl" "root_bucket" {
+  bucket = aws_s3_bucket.root_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "root_bucket" {
+  bucket = aws_s3_bucket.root_bucket.id
+
+  redirect_all_requests_to {
+    host_name = "https://www.${var.domain_name}"
   }
 }
